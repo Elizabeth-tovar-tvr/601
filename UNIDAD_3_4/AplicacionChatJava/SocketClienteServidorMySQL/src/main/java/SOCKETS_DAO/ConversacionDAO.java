883b433b9a6996f8d030d21a7828abcacc91899c
Elizabeth-tovar-tvr <et123897@gmail.com>
2023-07-30 20:56:57 -0600

    Este es el dao de l Conversación, ojo

---
+++ UNIDAD_3_4/AplicacionChatJava/SocketClienteServidorMySQL/src/main/java/SOCKETS_DAO/ConversacionDAO.java
@@ -0,0 +1,13 @@
+package SOCKETS_DAO;
+
+/**
+ *
+ * @author et123
+ */
+public interface ConversacionDAO {
+
+    public void registrarConversacionA(String mensaje);
+
+    public void registrarConversacionB(String mensaje);
+
+}
